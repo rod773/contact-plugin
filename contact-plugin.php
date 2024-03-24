cbe5674e2f933f98731870b43629dfee33c0a804
rod773 <rodrigoarcadio1@gmail.com>
2024-03-24 15:29:05 +0100

    initial

---
+++ contact-plugin.php
@@ -0,0 +1,20 @@
+<?php
+
+/***
+ * Plugin Name: Contact Plugin
+ * Author: Rodrigo
+ * Description: Contact Plugin
+ * Verions: 0.0.1
+ */
+
+ if(!defined('ABSPATH')){
+    die('You  can not be here');
+ }
+
+
+ if(!class_exists('ContactPlugin')){
+
+    class ContactPlugin{
+        
+    }
+ }
