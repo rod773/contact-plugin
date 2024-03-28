06ae1d5fd097f8f64fc7bde392090b3ad68a58b9
rod773 <104315743+rod773@users.noreply.github.com>
2024-03-28 12:48:12 +0100

    update

--- includes/options-page.php
+++ includes/options-page.php
@@ -1,7 +1,7 @@
 <?php
 
 
-add_action( 'after_seup_theme','load_carbon_fields');
+add_action( 'after_setup_theme','load_carbon_fields');
 
 function load_carbon_fields(){
     Carbon_Fields::boot();
