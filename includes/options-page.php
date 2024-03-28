201b8c31a763d088d2d14c23c6affdcebc96d253
rod773 <104315743+rod773@users.noreply.github.com>
2024-03-28 12:59:55 +0100

    initial

--- includes/options-page.php
+++ includes/options-page.php
@@ -1,8 +1,8 @@
 <?php
 
 
-add_action( 'after_setup_theme','load_carbon_fields');
+// add_action( 'after_setup_theme','load_carbon_fields');
 
-function load_carbon_fields(){
-    Carbon_Fields::boot();
-}
+// function load_carbon_fields(){
+//     Carbon_Fields::boot();
+// }
